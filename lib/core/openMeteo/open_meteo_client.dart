1fb782108f475a6cc204ba15d43ed1cb41b308af
flyingV805 <petrov.vladislav1992@gmail.com>
2025-04-02 21:15:59 +0500

    Working on UI

--- lib/core/openMeteo/open_meteo_client.dart
+++ lib/core/openMeteo/open_meteo_client.dart
@@ -4,7 +4,7 @@ import 'package:retrofit/retrofit.dart';
 
 part 'open_meteo_client.g.dart';
 
-@RestApi(baseUrl: 'https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/')
+@RestApi(baseUrl: 'https://api.open-meteo.com/v1/forecast')
 abstract class OpenMeteoClient {
 
   factory OpenMeteoClient(Dio dio, {String? baseUrl}) = _OpenMeteoClient;
