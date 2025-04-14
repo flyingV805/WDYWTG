530b2c5edd35e0569f106fb0eefbf8f2b92fc345
flyingV805 <petrov.vladislav1992@gmail.com>
2025-04-14 16:16:54 +0500

    UserLocation as separate feature with separate BLoC

--- lib/features/featureUserLocation/bloc/ul_bloc.dart
+++ lib/features/featureUserLocation/bloc/ul_bloc.dart
@@ -4,6 +4,15 @@ import 'package:wdywtg/features/featureUserLocation/bloc/ul_state.dart';
 
 class UserLocationBloc extends Bloc<UserLocationEvent, UserLocationState>{
 
-  UserLocationBloc(): super(UserLocationState.empty());
+  UserLocationBloc(): super(UserLocationState.empty()){
+    on<Initialize>(_startRoutine);
+  }
+
+  Future<void> _startRoutine(
+    Initialize event,
+    Emitter<UserLocationState> emit
+  ) async {
+
+  }
 
 }
