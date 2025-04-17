b1ecb345f532e1890555376486e1f85905d81482
flyingV805 <petrov.vladislav1992@gmail.com>
2025-04-17 21:14:01 +0500

    SavedPlaces db tested - it's something

--- lib/features/featureFind/repository/add_place_repository_impl.dart
+++ lib/features/featureFind/repository/add_place_repository_impl.dart
@@ -1,11 +1,8 @@
 import 'package:wdywtg/features/featureFind/model/add_result.dart';
-
 import 'package:wdywtg/features/featureFind/model/place_suggestion.dart';
-
 import 'add_place_repository.dart';
 
 class AddPlaceRepositoryImpl extends AddPlaceRepository {
-
 
   @override
   Future<AddResult> addSavedPlace(PlaceSuggestion suggestion) {
