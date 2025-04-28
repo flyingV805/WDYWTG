5ce5923ecc11d176bed757605512fc34e3db2f5f
flyingV805 <petrov.vladislav1992@gmail.com>
2025-04-28 17:36:34 +0500

    Sync

--- lib/core/unsplash/image_palette.dart
+++ lib/core/unsplash/image_palette.dart
@@ -5,6 +5,8 @@ import 'package:wdywtg/commonModel/place_picture_palette.dart';
 Future<PlacePicturePalette> findTextPalette(String imageUrl) async {
 
   final palette = await PaletteGenerator.fromImageProvider(NetworkImage(imageUrl));
+
+  // 0.299*R + 0.587*G + 0.114*B
   final textColor = ThemeData.estimateBrightnessForColor(palette.dominantColor?.color ?? Colors.white);
 
   return switch(textColor){
